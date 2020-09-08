69493bd93b48e7233f30cc4ddfc432234cb284be
Yevhenii <kz.greenlinefamily@gmail.com>
2020-09-08 23:16:05 +0100

    lessons 1-12. Create react app. Components and grid introduction

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="" alt=""/>
+            </header>
+
+            <nav className='app-navigation'>
+                <ul>
+                    <li>
+                        <a href="#">Profile</a>
+                    </li>
+                    <li>
+                        <a href="#">Messages</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <li>
+                        <a href="#">Music</a>
+                    </li>
+                    <li>
+                        <a href="#">Settings</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <section className='main-content'>
+                <div>banner</div>
+                <div>ava + desc</div>
+                <ul>
+                    <li>post1</li>
+                    <li>post2</li>
+                </ul>
+            </section>
+
+        </div>
+    )
 }
 
 export default App;
