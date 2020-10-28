4f322a3d873dd5ec664c9591881f72ebdc707ee2
Yevhenii <kz.greenlinefamily@gmail.com>
2020-10-28 18:10:55 +0100

    Lesson 54. Lifecycle methods. ComponendDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,15 +5,11 @@ import * as axios from "axios";
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items)
         })
-
     }
-
 
     render() {
         return (
