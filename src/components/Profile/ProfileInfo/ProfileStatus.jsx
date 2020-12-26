d424d1e5dce6d3d16b594f8742eb74af499a904b
Yevhenii <kz.greenlinefamily@gmail.com>
2020-12-26 22:59:56 +0100

    Lesson 74. ComponentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -27,6 +27,14 @@ class ProfileStatus extends React.Component {
         })
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
         return (
             <div>
