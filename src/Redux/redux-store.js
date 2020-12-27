c6814c273e55728bd0cd2947e9bce15087604adf
Yevhenii <kz.greenlinefamily@gmail.com>
2020-12-27 01:56:55 +0100

    Lesson 75. Redux-form start

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -1,15 +1,17 @@
 import {applyMiddleware, combineReducers, createStore} from "redux"
-import dialogsReducer from "./dialogsReducer";
-import profileReducer from "./profileReducer";
-import usersReducer from "./usersReducer";
-import authReducer from "./authReducer";
-import thunkMiddleware from "redux-thunk";
+import dialogsReducer from "./dialogsReducer"
+import profileReducer from "./profileReducer"
+import usersReducer from "./usersReducer"
+import authReducer from "./authReducer"
+import { reducer as formReducer } from 'redux-form'
+import thunkMiddleware from "redux-thunk"
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
-    authData: authReducer
+    authData: authReducer,
+    form: formReducer
 })
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware))
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,42 @@
 import React from "react"
+import {Field, reduxForm} from "redux-form"
 
 const LoginPage = (props) => {
-    return <h1>Login Page</h1>
+
+    const onSubmit = (formData) => {
+
+    }
+
+    return <div>
+        <h1>Login</h1>
+
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
 }
+
+const LoginForm = (props) => {
+    return <form action="#" onSubmit={props.handleSubmit}>
+        <div>
+            <Field component={'input'} name={'login'} type="text" placeholder={'Login'}/>
+        </div>
+        <div>
+            <Field component={'input'} name={'password'} type="password" placeholder={'Password'}/>
+        </div>
+        <div>
+            <label>
+                <Field component={'input'} name={'rememberMe'} type="checkbox"/> Remember me
+            </label>
+        </div>
+        <div>
+            <button>Submit</button>
+        </div>
+    </form>
+}
+
+const LoginReduxForm = reduxForm({
+    form: 'login'
+})(LoginForm)
+
+
 
 export default LoginPage
