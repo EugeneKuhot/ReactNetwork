ad16ecd222c43ed18ccec6273fcc3c3d13d10b21
Yevhenii <kz.greenlinefamily@gmail.com>
2021-01-05 08:43:25 +0100

    Lesson 85. Hook useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,10 +1,15 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import s from '../Profile.module.css'
 
 const ProfileStatusWithHooks = (props) => {
 
     let [editMode, setEditMode] = useState(false)
     let [status, setStatus] = useState(props.status)
+
+    useEffect( () => {
+        setStatus(props.status)
+        }, [props.status]
+    )
 
     const activateEditMode = () => {
         setEditMode(true)
